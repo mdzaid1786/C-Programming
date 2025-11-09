dd19e750177951469395bcbb38931ba3190aa3d3
mdzaid1786 <smzaid12567@gmail.com>
2025-11-09 13:30:28 +0530

    Create Exp_5.c

---
+++ Exp_5.c
@@ -0,0 +1,18 @@
+//Name :- Zaid Shaikh
+//Div :- D
+//Roll no :- 31
+//Uin :- 251P108
+//Branch :- computer engineering 
+
+#include <stdio.h>
+int factorial(int n) {
+ if (n == 0 || n == 1) // base case
+ return 1;
+ else
+ return n * factorial(n - 1); // recursive case
+}
+int main() {
+ int num = 5;
+ printf("Factorial of %d is %d\n", num, factorial(num));
+ return 0;
+}
